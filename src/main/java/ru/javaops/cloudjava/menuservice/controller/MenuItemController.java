838bbd0fe96f9caca0dede7a445354e36cad1b19
Zheka Slepko <zhenya.slepko@gmail.com>
2024-09-06 20:13:58 +0300

    feat: создать централизованный обработчик исключений.

--- src/main/java/ru/javaops/cloudjava/menuservice/controller/MenuItemController.java
+++ src/main/java/ru/javaops/cloudjava/menuservice/controller/MenuItemController.java
@@ -1,10 +1,11 @@
 package ru.javaops.cloudjava.menuservice.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.javaops.cloudjava.menuservice.dto.CreateMenuRequest;
 import ru.javaops.cloudjava.menuservice.dto.MenuItemDto;
@@ -23,4 +24,37 @@ public class MenuItemController {
 
     private final MenuService menuService;
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MenuItemDto createMenuItem(@RequestBody @Valid CreateMenuRequest dto) {
+        log.info("Received POST request to create MenuItem: {}", dto);
+        return menuService.createMenuItem(dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMenuItem(@PathVariable("id") @Positive(message = "id должен быть > 0") Long id) {
+        log.info("Received request to DELETE MenuItem with id={}", id);
+        menuService.deleteMenuItem(id);
+    }
+
+    @PatchMapping("/{id}")
+    public MenuItemDto updateMenuItem(@PathVariable("id") @Positive(message = "id должен быть > 0.") Long id,
+                                      @RequestBody @Valid UpdateMenuRequest update) {
+        log.info("Received PATCH request to update MenuItem with id={}. Update params: {}", id, update);
+        return menuService.updateMenuItem(id, update);
+    }
+
+    @GetMapping("/{id}")
+    public MenuItemDto getMenu(@PathVariable("id") @Positive(message = "id должен быть > 0.") Long id) {
+        log.info("Received request to GET MenuItem with id={}", id);
+        return menuService.getMenu(id);
+    }
+
+    @GetMapping
+    public List<MenuItemDto> getMenus(@RequestParam("category") @NotBlank(message = "Категория не должна быть пустой.") String category,
+                                      @RequestParam(value = "sort", defaultValue = "az") @NotBlank(message = "Параметр сортировки не должен быть пустым.") String sort) {
+        log.info("Received request to GET list of MenuItems for category={}, sorted by={}", category, sort);
+        return menuService.getMenusFor(Category.fromString(category), SortBy.fromString(sort));
+    }
 }
